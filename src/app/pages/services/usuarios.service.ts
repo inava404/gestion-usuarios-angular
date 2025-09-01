b3e572af38ab4ee25de27460a7fbbd84a70b4c63
inava <144198990+inava404@users.noreply.github.com>
2025-08-31 23:52:43 -0600

    Paso 5 - API pública en Usuarios.Service.ts

--- src/app/pages/services/usuarios.service.ts
+++ src/app/pages/services/usuarios.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  private apiUrl = 'https://jsonplaceholder.typicode.com/users';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  obtenerUsuarios(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
 }
