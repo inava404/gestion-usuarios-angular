b6c66b894bdf8cdf0da89de002a9430d783e457d
inava <144198990+inava404@users.noreply.github.com>
2025-08-31 23:43:38 -0600

    Paso 4 Hecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    // Redirige la ruta raíz a /home
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    // Carga el PagesModule de forma diferida
+    loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PerfilComponent } from './perfil/perfil.component';
+import { EstructurasComponent } from './estructuras/estructuras.component';
+import { DocumentacionComponent } from './documentacion/documentacion.component';
+import { UsuariosComponent } from './usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'perfil',
+    component: PerfilComponent
+  },
+  {
+    path: 'estructuras',
+    component: EstructurasComponent
+  },
+  {
+    path: 'documentacion',
+    component: DocumentacionComponent
+  },
+  {
+    path: 'usuarios',
+    component: UsuariosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
