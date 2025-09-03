e81e65b999eadae1d71f2b56073fee344c351c50
Xanaks15 <josue.torresz@alumno.buap.mx>
2025-09-02 18:13:03 -0600

    perfil y estructuras funcionando

--- src/app/pages/estructuras/estructuras.component.ts
+++ src/app/pages/estructuras/estructuras.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./estructuras.component.css']
 })
 export class EstructurasComponent {
+  // Para *ngIf
+  mostrarMensaje = true;
 
+  // Para *ngFor
+  tecnologias = ['Angular', 'TypeScript', 'NodeJS', 'Ionic', 'NPM'];
+
+  // Para *ngSwitch
+  opcion = 'angular'; // valor por defecto
 }
+  
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent {
-
+  usuario = {
+    nombre: 'Juan Pérez',
+    email: 'juan.perez@example.com',
+    telefono: '+52 222 123 4567',
+    direccion: 'Av. Reforma 123, Puebla, México',
+    fechaNacimiento: '1999-08-15'
+  };
 }
